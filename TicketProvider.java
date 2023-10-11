ff509ae366f70dae3dada975ff8052611e56ea75
Svetlana Riasnova <R_S_U@outlook.com>
2023-10-11 15:45:57 +0300

    добален код для класса ticketprovider

---
+++ TicketProvider.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class TicketProvider {
+    public List<Ticket> getTickets(int routeNumber) {
+        return ticketRepo.readAll(routeNumber);
+    }
+    public boolean updateTickets(Ticket ticket) {
+        ticket.setValid(false);
+        return ticketRepo.update(ticket);
+    }
+}
