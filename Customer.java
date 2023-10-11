f3d0605b5e70689eb074085dab7c18c2eb1623e6
Svetlana Riasnova <R_S_U@outlook.com>
2023-10-11 15:33:38 +0300

    добален код для класса сustomer

---
+++ Customer.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Customer {
+
+    public int id;
+    public CashProvider Cash;
+    List<Ticket> getAllTickets();
+
+   private boolean buyTicket(Ticket ticket) {
+    //покупка билета успешно -вернет true
+   }
+
+   public void searchTicket(dateTime date, int rootNumber) {
+
+   }
+
+   private boolean reserveTichet(Ticket ticket) {
+    // добавлен в рамках дз. Бронирование билета
+    // если isValid true ставит бронь
+    }
+    
+}
